2693cdc93e104f116b619956fdf18b2759505171
CHERRY <ty_1_ta@mail.ru>
2024-01-24 12:57:00 +0700

    Fix

--- ServerMessenger/ServerMessenger.cpp
+++ ServerMessenger/ServerMessenger.cpp
@@ -1,24 +1,35 @@
 ﻿#include "pch.h"
 
 #include "Server.h"
-#include <iostream>
-static std::string incomingMessage;
+#include <conio.h>
+static std::wstring incomingMessage;
 int wmain(int, wchar_t* [], wchar_t* [])
 {
     SetConsoleOutputCP(CP_UTF8);
+    static CONSOLE_SCREEN_BUFFER_INFO csbi;
     spdlog::set_default_logger(std::make_shared<spdlog::logger>("main", spdlog::sinks_init_list
         {
-            std::make_shared<spdlog::sinks::callback_sink_mt>([](const spdlog::details::log_msg& msg)
+            std::make_shared<spdlog::sinks::callback_sink_mt>([](const spdlog::details::log_msg&)
                 {
-                    static SHORT cp;
-                    COORD cursorPosition;
-                    cursorPosition.X = 0;
-                    cursorPosition.Y = -1;
-                    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), cursorPosition);
-                    std::cout << incomingMessage;
+                    printf_s("%s", "\x1b[2K\r");
+                    static CONSOLE_SCREEN_BUFFER_INFO csbi2;
+                    GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi2);
+                    for (short i = 0; i < csbi2.dwCursorPosition.Y - csbi.dwCursorPosition.Y; i++)
+                    {
+                        printf_s("%s", "\x1b[A\r");
+                        printf_s("%s", "\x1b[2K\r");
+                    }
                 }),
             std::make_shared<spdlog::sinks::stdout_color_sink_mt>(),
-            std::make_shared<spdlog::sinks::rotating_file_sink_mt>(L"logs/log.txt", 1024Ui64 * 1024Ui64 * 16Ui64, 10Ui64)
+            std::make_shared<spdlog::sinks::rotating_file_sink_mt>(L"logs/log.txt", 1024Ui64 * 1024Ui64 * 16Ui64, 10Ui64),
+            std::make_shared<spdlog::sinks::callback_sink_mt>([](const spdlog::details::log_msg&)
+                {
+                    printf_s("%s", "> ");
+                    std::string buf;
+                    GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi);
+                    spdlog::details::os::wstr_to_utf8buf(incomingMessage, buf);
+                    printf_s("%s", buf.c_str());
+                })
         }));
     sqlite3* database;
     int nRes = sqlite3_open_v2("server.sqlite3", &database, SQLITE_OPEN_CREATE | SQLITE_OPEN_READWRITE, NULL);
@@ -47,11 +58,6 @@ int wmain(int, wchar_t* [], wchar_t* [])
     const std::string& str = buffer.GetString();
     rapidjson::Document document2;
     document2.Parse(str.c_str());
-    std::string str2("INSERT INTO USERS VALUES(?, ?, ?");
-    /*str2 += document2["kyber_pk"].GetString();
-    str2 += "', '";
-    str2 += document2["dilithium_pk"].GetString();
-    str2 += "');";*/
     sqlite3_stmt* stmW;
     nRes = sqlite3_prepare_v2(database, "INSERT OR IGNORE INTO USERS VALUES(?, ?, ?)", -1, &stmW, 0);
     if (nRes != SQLITE_OK) { return nRes; }
@@ -71,23 +77,38 @@ int wmain(int, wchar_t* [], wchar_t* [])
     std::thread([]() {
         while (true)
         {
-            spdlog::info("haha noob");
+            spdlog::info(L"хай");
+            std::this_thread::sleep_for(std::chrono::seconds(2));
         }}).detach();
-    const int bufferSize = 256;
-    while (true) {
-        char c;
-        std::cin.get(c);
-
-        if (c == '\n') {
-            // Обработка перевода строки
-        }
-        else if (c == '\r') {
-            // Обработка уведомления о входящем сообщении
-            std::cout << "\r" << std::string(bufferSize, ' ') << "\r" << incomingMessage << std::endl;
-        }
-        else {
-            // Добавление символа в буфер
-            incomingMessage += c;
+    while (true)
+    {
+        std::wstring wbuf;
+        std::string buf;
+        wbuf.push_back(_getwch());
+        spdlog::details::os::wstr_to_utf8buf(wbuf, buf);
+        switch (wbuf[0])
+        {
+        case '\0':
+        case '\n':
+            break;
+        case '\b':
+            if (!incomingMessage.empty())
+            {
+                printf_s("%s", "\b \b");
+                incomingMessage.pop_back();
+            }
+            break;
+        case '\r':
+            if (!incomingMessage.empty())
+            {
+                printf_s("%s", "\n> ");
+                incomingMessage.clear();
+            }
+            break;
+        default:
+            printf_s("%s", buf.c_str());
+            incomingMessage.append(wbuf);
+            break;
         }
     }
     std::shared_ptr<Server> server = std::make_shared<Server>();
